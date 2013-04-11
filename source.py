885935efa05e49e4503ea2a930fdcee9814bb7bc
Gavin Bird <gavin@birdfamily.us>
2013-04-10 20:27:17 -0700

    wrote source.py

--- source.py
+++ source.py
@@ -18,23 +18,41 @@ class Source:
             # Form the databits, from the filename 
             if self.fname is not None:
                 if self.fname.endswith('.png') or self.fname.endswith('.PNG'):
-                    # Its an image
+                    #A Assume it's an image
+                    databits = self.bits_from_image(filename)
+                    payload = databits + self.get_header(os.path.getsize(filename)*8, "image")
                 else:           
-                    # Assume it's text                    
+                    # Assume it's text 
+                    databits = self.text2bits(filename)   
+                    payload = databits + self.get_header(os.path.getsize(filename)*8, "text")                
             else:               
                 # Send monotone (the payload is all 1s for 
                 # monotone bits)   
+                databits = [1]*monotone
+                payload = databits + self.get_header(monotone, "monotone")
+
             return payload, databits
 
     def text2bits(self, filename):
         # Given a text file, convert to bits
+        f = open(filename)
+        s = s.read()
+        bits = int(s, 2)
         return bits
 
     def bits_from_image(self, filename):
         # Given an image, convert to bits
-        return bits
+        f = Image.open(filename)
+        return f.bits
 
     def get_header(self, payload_length, srctype): 
         # Given the payload length and the type of source 
         # (image, text, monotone), form the header
+        header = bin(x)
+        if srctype == "image":
+            header = "00" + header
+        else if srctype = "text":
+            header = "01" + header
+        else if srctype = "monotone":
+            header = "10" + header
         return header
